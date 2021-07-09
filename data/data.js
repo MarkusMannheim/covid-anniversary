77f7141b991c8765aecfe80cb3fc3093cb2f6aa6
Markus Mannheim <markus@mannheim.media>
2021-07-10 08:35:30 +1000

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -14,17 +14,55 @@ fs.readFile("./areas.geojson", "utf8", function(error, data) {
       if (i > -1) {
         d.properties.name = d.properties.name.slice(0, i - 1);
       }
+      if (d.properties.code == "89399") {
+        d.properties.name = "ACT";
+      }
       return d;
     });
-
-  geoData.forEach(function(d) {
-    console.log(d.properties);    
-  });
 
   fs.readFile("./cases.csv", "utf8", function(error, data) {
     if (error) throw error;
 
-    caseData = d3.csvParse(data);
+    caseData = d3.csvParse(data)
+      .map(function(d) {
+        return {
+          date: d.date,
+          code: d.code,
+          recent: Math.round(+d.recent)
+        };
+      });
 
+    finalData = {
+      type: "FeatureCollection",
+      features: []
+    };
+
+    codes = Array.from(new Set(caseData.map(function(d) { return d.code; })));
+    codes.forEach(function(d) {
+      let dataMatch = caseData
+        .filter(function(e) {
+          return e.code == d;
+        });
+      let geoMatch = geoData
+        .filter(function(e) {
+          return e.properties.code == d;
+        });
+      if (geoMatch.length == 1) {
+        finalData.features.push({
+          type: "Feature",
+          geometry: geoMatch[0].geometry,
+          properties: {
+            name: geoMatch[0].properties.name,
+            data: dataMatch.map(function(e) {
+              return e.recent;
+            })
+          }
+        });
+      }
+    });
+
+    fs.writeFile("./lga.geojson", JSON.stringify(finalData), function(error) {
+      console.log("lga.geojson written");
+    });
   });
 });
